cddb901a4441f526bee15345cb123af69a0dc0e8
Karan143246 <103922464+Karan143246@users.noreply.github.com>
2022-10-20 19:37:06 +0530

    created

---
+++ EqualNum.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+int main(){
+    int a,b;
+    std::cout <<"Enter first number :";
+    std::cin >>a;
+    std::cout <<"Enter second number :";
+    std::cin >>b;
+    if(a==b){
+        std::cout <<"Both numbers are equal .";
+    }
+    else{
+        std::cout << "Both numbers are not equal . ";
+    }
+}
